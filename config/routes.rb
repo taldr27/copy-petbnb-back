28255fd7a780ee80c545be73f129cd6fa348f461
lana <diegogarciaq98@gmail.com>
2023-03-15 13:53:13 -0500

    root to the top

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root "pet_rooms#public"
   mount Rswag::Ui::Engine => '/api-docs'
   mount Rswag::Api::Engine => '/api-docs'
   get '/current_user', to: 'current_user#index'
@@ -19,5 +20,4 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "pet_rooms#public"
 end
